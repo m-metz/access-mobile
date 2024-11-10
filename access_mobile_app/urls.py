e361a5f7f42ca385b5ac4bdda6fa5518156bb9a7
JonahRichards <jonah.richards@ucalgary.ca>
2024-11-10 07:55:34 -0700

    Working donee login

--- access_mobile_app/urls.py
+++ access_mobile_app/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('access-mobile/', views.access_mobile, name='access_mobile'),
-    path('donatee-signin/', views.donatee_signin, name='donatee_signin'),
+    path('donee-signin/', views.donee_signin, name='donee_signin'),
     path('donee/', views.donee, name='donee'),
-    path('donee-dashboard/', views.donee_dashboard, name='donee_dashboard'),
+    path('donee-dashboard/<str:sim>', views.donee_dashboard, name='donee_dashboard'),
     path('donor/', views.donor, name='donor'),
     path('donor-signin/', views.donor_signin, name='donor_signin'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
--- access_mobile_app/views.py
+++ access_mobile_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.timezone import datetime
+from .models import DoneeAccount
 
 # from django.shortcuts import render, redirect
 # from django.contrib.auth import login, authenticate
@@ -9,8 +10,17 @@ from django.utils.timezone import datetime
 def landing_page(request):
     return render(request, 'access_mobile_app/landing_page.html')
 
-def donatee_signin(request):
-    return render(request, 'access_mobile_app/donatee_signin.html')
+def donee_signin(request):
+    if request.method == 'POST':
+        sim = request.POST.get('sim')
+        try:
+            donee = DoneeAccount.objects.get(sim=sim)
+            # Redirect to the donee dashboard page with the correct SIM in the URL
+            return redirect('donee_dashboard', sim=donee.sim)
+        except DoneeAccount.DoesNotExist:
+            # If SIM doesn't exist, show an error message
+            return render(request, 'access_mobile_app/donee_signin.html', {'error': 'Non-existent SIM'})
+    return render(request, 'access_mobile_app/donee_signin.html')
 
 def donor_signin(request):
     return render(request, 'access_mobile_app/donor_signin.html')
@@ -21,8 +31,12 @@ def access_mobile(request):
 def donee(request):
     return render(request, 'access_mobile_app/donee.html')
 
-def donee_dashboard(request):
-    return render(request, 'access_mobile_app/donee_dashboard.html')
+def donee_dashboard(request, sim):
+    try:
+        donee = DoneeAccount.objects.get(sim=sim)
+    except DoneeAccount.DoesNotExist:
+        return redirect('donatee_signin')  # Redirect back to sign-in if SIM is not found
+    return render(request, 'access_mobile_app/donee_dashboard.html', {'donee': donee})
 
 def edit_profile(request):
     return render(request, 'access_mobile_app/edit_profile.html')
