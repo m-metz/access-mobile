207341dfbfd731d1d44b4ced0e737dedb3d3a115
JonahRichards <jonah.richards@ucalgary.ca>
2024-11-10 10:18:57 -0700

    Donor Dashboard

--- access_mobile_app/forms.py
+++ access_mobile_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import DoneeAccount
+from .models import DoneeAccount, DonorAccount
 
 class DoneeEditForm(forms.ModelForm):
     class Meta:
         model = DoneeAccount
         fields = ['name', 'bio']
+
+class DonorProfileForm(forms.ModelForm):
+    class Meta:
+        model = DonorAccount
+        fields = ['name', 'bio']  # Allow editing name and bio
