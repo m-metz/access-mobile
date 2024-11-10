fbcaf5ebdc2f919251c676579b439d929b89864f
EthanSCoulthard <187909442+esCoulthard@users.noreply.github.com>
2024-11-10 06:30:05 -0700

    added views

--- access_mobile_app/views.py
+++ access_mobile_app/views.py
@@ -14,3 +14,18 @@ def donatee_signin(request):
 
 def donor_signin(request):
     return render(request, 'access_mobile_app/donor_signin.html')
+
+def access_mobile_view(request):
+    return render(request, 'access_mobile.html')
+
+def donee_view(request):
+    return render(request, 'donee.html')
+
+def donee_dashboard_view(request):
+    return render(request, 'donee_dashboard.html')
+
+def edit_profile_view(request):
+    return render(request, 'edit_profile.html')
+
+def sign_up_view(request):
+    return render(request, 'sign_up.html')
